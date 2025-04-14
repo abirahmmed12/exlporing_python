7048192d0f903980f34d704df5c895cac1128709
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2025-04-14 13:12:07 +0600

    adding

--- practicing_loop.py
+++ practicing_loop.py
@@ -1,10 +1,17 @@
-my_list ={"name":"abir",
-          "age":25,
-          "favourite_color":"black",
-          "brand":"apple"}
+# my_list ={"name":"abir",
+#           "age":25,
+#           "favourite_color":"black",
+#           "brand":"apple"}
 
-print(len(my_list))
+# print(len(my_list))
 
-for key, value in my_list.items():
-    print(key , value)
+# for key, value in my_list.items():
+#     print(key , value)
+
+my_list=['abir','mridul','soaib','badhon']
+for item in range(len(my_list)):
+    result=my_list[2]
     
+    
+print(result)
+  
