7bb6452d6b6d16f937b9ea7f54fd3a1e682acdcc
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2025-04-14 12:06:53 +0600

    adding

---
+++ practicing_loop.py
@@ -0,0 +1,10 @@
+my_list ={"name":"abir",
+          "age":25,
+          "favourite_color":"black",
+          "brand":"apple"}
+
+print(len(my_list))
+
+for value in my_list:
+    print(value)
+    
