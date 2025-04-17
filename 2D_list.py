639a7535074100ec1c01ca37391eb3e4ee8c51fc
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2025-04-17 11:18:23 +0600

    function listmethod

--- 2D_list.py
+++ 2D_list.py
@@ -1,2 +1,8 @@
-my_list=[[45,65,12],[55,69,22],[34,32,44]]
-print(my_list[1][1])
+my_list=[2,34,9,5]
+my_list.sort()
+print(my_list)
+
+my_value={"name":"abir",
+          "age":33}
+for key,value in my_value.items():
+    print(key,value)
---
+++ funtion.py
@@ -0,0 +1,32 @@
+# def substraction(a,b):
+#     result=a+b
+#     return result
+# number_1=int(input("enter 1"))
+# number_2=int(input("enter 2"))
+# fol =substraction(number_1,number_2)
+# print(fol)
+
+# my_list=[3,4,5,6]
+# newList=my_list.copy()
+# print(newList)
+
+# list_1=[1,3,4,5,6]
+# list_2=[7,8,9,10,11]
+# list_1.extend(list_2)
+# print(list_2)
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.index("cherry") 
+print(x)
+
+y=[4,5,6,7,8]
+y.pop(1)
+print(y)
+y.insert(2,6)
+y.remove(7)
+print(y)
+y.reverse()
+print(y)
+y.sort()
+print(y)
